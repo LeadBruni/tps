2c39d22dc4b29010daeecf4f668ea596615535d6
Acer <Acer@ACER-PC>
2016-11-02 18:46:18 -0300

    lo de muri fina

--- TP4/TP4/Program.cs
+++ TP4/TP4/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ej1
+namespace TP4
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CapaVista vista = new CapaVista();
+            vista.Ejecutar();
+            Console.ReadKey();
         }
     }
 }
